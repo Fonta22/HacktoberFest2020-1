b26185d7e69e53c300981914c2b5d182196b40ce
Fonta22 <61181201+Fonta22@users.noreply.github.com>
2020-10-09 20:33:46 +0200

    Create linear-constant.py
    Added a basic program that calculates the constant of proportionality of a linear function.

---
+++ Algorithms/Python/linear-constant.py
@@ -0,0 +1,8 @@
+# Program that calculates the constant of proportionality of a linear function.
+# y = kx
+
+def linear(x, y):
+    k = x / y
+    print(f'The constant of proportionality of this function is {k}')
+
+linear(7,5) # By Fonta22
